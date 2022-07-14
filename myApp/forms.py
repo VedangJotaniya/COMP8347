3ba1491878e2b3d79989048c5283a03d8c551e52
VedangJotaniya <vedangjotaniya@gmail.com>
2022-07-14 11:27:05 -0400

    lab 08 commit #1

--- myApp/forms.py
+++ myApp/forms.py
@@ -5,4 +5,14 @@ from myApp.models import Order
 class InterestForm(forms.Form):
     interested = forms.RadioSelect()
     levels = forms.IntegerField(min_value=1, max_value=10, initial=1)
-    comments = forms.Textarea()
+    comments = forms.Textarea
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = "__all__"
+        widgets = {
+            'student': forms.RadioSelect,
+            'order_date': forms.SelectDateWidget
+        }
+
--- myApp/views.py
+++ myApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Topic, Course, Student, Order
 from django.shortcuts import get_object_or_404
-
+from .forms import *
 
 # Create your views here.
 
@@ -32,3 +32,38 @@ def courses(request):
     course_list = Course.objects.all().order_by('id')
     return render(request, 'myApp/courses.html', {'course_list': course_list})
 
+def place_order(request):
+    msg = ''
+    courlist = Course.objects.all()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+
+            if order.course.price > 150:
+                order.course.discount()
+
+            if order.levels <= order.course.stages:
+                order.save()
+                msg = 'Your course has been ordered successfully. '
+            else:
+                msg = 'You exceeded the number of levels for this course.'
+            return render(request, 'myapp/order_response.html', {'msg': msg})
+    else:
+        form = OrderForm()
+    return render(request, 'myapp/placeorder.html', {'form': form, 'msg': msg, 'courlist': courlist})
+
+
+def coursedetail(request, cour_id):
+    course = Course.objects.get(id=cour_id)
+    if request.method == 'POST':
+        form = InterestForm(request.POST)
+        if form.is_valid() and form.cleaned_data['interested'] == 1:
+            course.interested = course.interested + 1
+            course.save()
+            msg = "Order was successful"
+            return render(request, '/myapp/order_response.html', {'msg': msg})
+    else:
+        form = InterestForm()
+    print(course)
+    return render(request, 'myapp/coursedetail.html', {'form': form, 'course': course})
